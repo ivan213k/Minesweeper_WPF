ebac37bea5a48a36c93f44f5d47f6958bdae89df
ivan213k <ivan213k@gmail.com>
2020-12-16 14:51:31 +0200

    Implemented base MVVM infrastructure

---
+++ Minesweeper_WPF/ViewModels/BaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Minesweeper_WPF.ViewModels
+{
+    abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnePropertyChanged([CallerMemberName] string propName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
---
+++ Minesweeper_WPF/ViewModels/Command.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Minesweeper_WPF.ViewModels
+{
+    abstract class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private readonly Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+
+        public void OneExecuteChaneged()
+        {
+            CanExecuteChanged?.Invoke(this, new EventArgs());
+        }
+
+        public Command(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
