e70388dd76b24b13b4f5f31494f946eee5193570
ivan213k <ivan213k@gmail.com>
2020-12-17 10:07:48 +0200

    Implemented Cell class

---
+++ Minesweeper_WPF.Core/Core/Cell.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Minesweeper_WPF.Core
+{
+    public class Cell
+    {
+        public bool IsBomb { get; private set; }
+
+        public bool IsEmpty { get; private set; }
+
+        public byte? Number { get; private set; }
+
+        public Cell()
+        {
+            IsEmpty = true;
+        }
+        public Cell(bool isBomb)
+        {
+            IsBomb = isBomb;
+        }
+        public Cell(byte num)
+        {
+            if (num >=1 && num<=8)
+            {
+                Number = num;
+            }
+            else
+            {
+                throw new ArgumentException("number can not be more then 8 and less then 1");
+            }
+            
+        }
+    }
+}
