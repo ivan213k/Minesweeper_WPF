e6dd59c3a39b33f7ad79deeef7a6e3acc29fcaf7
ivan213k <ivan213k@gmail.com>
2020-12-17 09:45:37 +0200

    Implemented GameConfiguration class

---
+++ Minesweeper_WPF.Core/Core/GameConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Minesweeper_WPF.Core.Abstractions;
+
+namespace Minesweeper_WPF.Core.Core
+{
+    class GameConfiguration : IGameConfiguration
+    {
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+        public int BombsCount { get; set; }
+
+        public GameConfiguration()
+        {
+            Rows = 9;
+            Columns = 9;
+            BombsCount = 10;
+        }
+        public GameConfiguration(int rows, int cols, int bombsCount)
+        {
+            Rows = rows;
+            Columns = cols;
+            BombsCount = bombsCount;
+        }
+    }
+}
---
+++ Minesweeper_WPF.Core/Core/Point.cs
@@ -0,0 +1,16 @@
+﻿using Minesweeper_WPF.Core.Abstractions;
+
+namespace Minesweeper_WPF.Core.Core
+{
+    public class Point : IPoint
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
