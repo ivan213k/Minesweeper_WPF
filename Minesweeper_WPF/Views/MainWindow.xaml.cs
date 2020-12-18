bd1299dc063ea8034391c81865ef5b6ecfd9a0d0
ivan213k <ivan213k@gmail.com>
2020-12-18 09:55:55 +0200

    Implemented main window design

--- Minesweeper_WPF/Views/MainWindow.xaml.cs
+++ Minesweeper_WPF/Views/MainWindow.xaml.cs
@@ -20,9 +20,71 @@ namespace Minesweeper_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        int rows = 9;
+        int cols = 9;
+
+        Button[,] buttonsMatrix;
         public MainWindow()
         {
             InitializeComponent();
+            buttonsMatrix = new Button[rows, cols];
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            InitializeButtonsMatrix();
+            InitializeGrid();
+        }
+        private void InitializeGrid()
+        {
+            foreach (var rowDefinition in CreateRowDefinitions(rows))
+            {
+                GridField.RowDefinitions.Add(rowDefinition);
+            }
+            foreach (var colDefinition in CreateColsDefinitions(cols))
+            {
+                GridField.ColumnDefinitions.Add(colDefinition);
+            }
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < cols; j++)
+                {
+
+                    Grid.SetRow(buttonsMatrix[i, j], i);
+                    Grid.SetColumn(buttonsMatrix[i, j], j);
+                    GridField.Children.Add(buttonsMatrix[i, j]);
+                }
+        }
+        private void InitializeButtonsMatrix()
+        {
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < cols; j++)
+                {
+                    buttonsMatrix[i, j] = new Button();
+                    buttonsMatrix[i, j].Name = "Button" + "row" + i + "col" + j;
+                }
+        }
+        private ColumnDefinition[] CreateColsDefinitions(int count)
+        {
+            ColumnDefinition[] colDefinitions = new ColumnDefinition[count];
+            for (int i = 0; i < count; i++)
+            {
+                colDefinitions[i] = new ColumnDefinition();
+            }
+            return colDefinitions;
+        }
+        private RowDefinition[] CreateRowDefinitions(int count)
+        {
+            RowDefinition[] rowDefinitions = new RowDefinition[count];
+            for (int i = 0; i < count; i++)
+            {
+                rowDefinitions[i] = new RowDefinition();
+            }
+            return rowDefinitions;
+        }
+
+        private void menuItemExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
